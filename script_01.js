4306d43a337eb8850fc10fb81985685f33963a67
David Rudolf <84070566+Rudi19881988@users.noreply.github.com>
2021-05-21 10:33:16 +0200

    Fkt. Grundrechenarten

--- script_01.js
+++ script_01.js
@@ -4,9 +4,27 @@
 0. a+b / a-b/ a*b / a/b  // ergebnis c
 1. Dateneingabe + -überprüfung :: 
 2. Auswahl Rechenart :: 
-3. Fkt. Grundrechenarten :: 
+3. Fkt. Grundrechenarten :: check!
 4. Ausgabe in Konsole :: check!
 */
+
+// Modul: Division a / b | Test
+ausgabe(dividieren(1,2));
+function dividieren(a,b) {
+    return a / b;
+}
+
+// Modul: Multiplikation a * b | Test
+//ausgabe(multiplikation(1,2));
+function multiplikation(a,b) {
+    return a * b;
+}
+
+// Modul: Subtraktion a - b | Test
+//ausgabe(subtrahieren(1,2));
+function subtrahieren(a,b) {
+    return a - b;
+}
 
 // Modul: Addition a + b | Test
 //ausgabe(addieren(1,2));
