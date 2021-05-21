c5cfe48c56a5b5f95642c8dd0dd2e1997055e5d4
David Rudolf <84070566+Rudi19881988@users.noreply.github.com>
2021-05-21 10:19:37 +0200

    Modul: Addition a + b | Test

--- script_01.js
+++ script_01.js
@@ -5,6 +5,17 @@
 1. Dateneingabe + -überprüfung :: 
 2. Auswahl Rechenart :: 
 3. Fkt. Grundrechenarten :: 
-4. Ausgabe in Konsole :: 
+4. Ausgabe in Konsole :: check!
 */
 
+// Modul: Addition a + b | Test
+//ausgabe(addieren(1,2));
+function addieren(a,b) {
+    return a + b;
+}
+
+// Modul: Konsolenausgabe | Test:
+//ausgabe("Hallo Welt");
+function ausgabe(outputStr) {
+    console.log(outputStr);
+}
